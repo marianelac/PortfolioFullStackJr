308872aeba602a7ea157d35c75ff1a2d897aa073
marianelac <104935206+marianelac@users.noreply.github.com>
2022-05-29 16:47:49 -0300

    Update portfolio.service.ts

--- FRONT-END-ANGULAR/src/app/servicios/portfolio.service.ts
+++ FRONT-END-ANGULAR/src/app/servicios/portfolio.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Persona } from '../interfaces/persona';
 
-//'https://api-arg-programa.herokuapp.com/api/persona';
+
 
 
 @Injectable({
